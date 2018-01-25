45cf2628a4d0f37357171891398d5198718ce0c6
muhahahahe <muhahahahe@gmail.com>
2018-01-25 13:13:27 +0100

    added usecaster to customdamage

--- src/com/gmail/berndivader/mythicmobsext/mechanics/CustomDamage.java
+++ src/com/gmail/berndivader/mythicmobsext/mechanics/CustomDamage.java
@@ -1,5 +1,6 @@
 package com.gmail.berndivader.mythicmobsext.mechanics;
 
+import io.lumine.xikage.mythicmobs.skills.SkillCaster;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 import com.gmail.berndivader.utils.Utils;
@@ -21,11 +22,13 @@ ITargetedEntitySkill {
 	boolean iabs;
 	boolean ip;
 	boolean p;
+	boolean uc;
 	boolean pcur;
 	boolean debug;
 	double dbd;
 	DamageCause cause;
 	String amount;
+	String ca;
 
 	public CustomDamage(String skill, MythicLineConfig mlc) {
 		super(skill, mlc);
@@ -39,9 +42,10 @@ ITargetedEntitySkill {
 		this.iabs = mlc.getBoolean(new String[] { "ignoreabsorbtion", "ignoreabs", "iabs" }, false);
 		this.ip = mlc.getBoolean(new String[] { "ignorepower", "ip" }, false);
 		this.p = mlc.getBoolean(new String[] { "percentage", "p" }, false);
+		this.uc = mlc.getBoolean(new String[] { "usecaster", "uc" }, false);
 		this.pcur = mlc.getBoolean(new String[] { "percentcurrent", "pcur", "pc" }, false);
-		String ca = mlc.getString(new String[] { "damagecause", "cause", "dc" }, "CUSTOM").toUpperCase();
-		dbd = mlc.getDouble(new String[] { "rangedamagebydistance", "rdbd" }, -7331D);
+		ca = mlc.getString(new String[] { "damagecause", "cause", "dc" }, "CUSTOM").toUpperCase();
+		this.dbd = mlc.getDouble(new String[] { "rangedamagebydistance", "rdbd" }, -7331D);
 		cause=DamageCause.CUSTOM;
 		for (DamageCause dc : DamageCause.values()) {
 			if (dc.toString().equals(ca)) {
@@ -56,13 +60,18 @@ ITargetedEntitySkill {
 	public boolean castAtEntity(SkillMetadata data, AbstractEntity t) {
 		if (t.isDead() || t.getHealth() <= 0.0 || data.getCaster().isUsingDamageSkill())
 			return false;
+		SkillCaster c = data.getCaster();
 		double dmg = Utils.randomRangeDouble(this.amount);
 		if (this.p) {
-			dmg = this.pcur ? t.getHealth() * dmg : t.getMaxHealth() * dmg;
+			if (this.uc) {
+				dmg = this.pcur ? c.getEntity().getHealth() * dmg : c.getEntity().getMaxHealth() * dmg;
+			} else {
+				dmg = this.pcur ? t.getHealth() * dmg : t.getMaxHealth() * dmg;
+			}
 		}
 		if (!this.ip)
 			dmg = dmg * data.getPower();
-		if (this.dbd>-7331D) {
+		if (dbd>-7331D) {
 			int dd=(int)Math.sqrt(Utils.distance3D(data.getCaster().getEntity().getLocation().toVector(), t.getBukkitEntity().getLocation().toVector()));
 			dmg-=(dmg*(dd*dbd));
 		}
